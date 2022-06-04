53692e2ca3c5808577e65b216fde232ba2316cbd
Digital Charlie <furman.jr@gmail.com>
2022-06-04 07:56:51 -0700

    better round of styling

--- main.js
+++ main.js
@@ -6,15 +6,20 @@ const softwareWork = document.getElementById('software-work')
 const createWorkArticles = (projects) => {
 	projects.forEach((project) => {
 		let projectToAdd = document.createElement('article')
-		let imagesForAdding = project.images.map(image => `<img src=${image.image} alt=${image.description}/>`)
+		let imagesForAdding = project.images.map(image => `<img class="work-image" src=${image.image} alt=${image.description}/>`)
 		projectToAdd.classList.add(project.type, "project")
 		projectToAdd.innerHTML = 
 			`
+			<div class="project-topline">
 			<h3>${project.project}</h3>
-			<p>${project.role}</p>
+			<p class="subheader">${project.role}</p>
+			</div>
 			<p>${project.description}</p>
 			${
-				project.type === 'software' ? `<p><a href="${project.github}" target="_blank">Check out the project on Github</a></p>` : ''
+				project.type === 'software' ? `<p><a href="${project.github}" target="_blank">Visit the site</a> or <a href="${project.github}" target="_blank">check out the project on Github</a></p>` : ''
+			}
+			${
+				project.type === 'campaign' ? `<p><a href="${project.github}" target="_blank">Read news coverage of the ${project.project}</a></p>` : ''
 			}
 			<div class="project-images">
 				${imagesForAdding.join('')}
