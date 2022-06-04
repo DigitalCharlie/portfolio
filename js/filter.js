529da87a92769cb0fbf7a54fff21b64ebd0e382c
Digital Charlie <furman.jr@gmail.com>
2022-06-04 08:12:15 -0700

    refactoring

--- js/filter.js
+++ js/filter.js
@@ -1,33 +1,6 @@
-const workDisplay = document.getElementById('work-container')
 const allWork = document.getElementById('all-work')
 const campaignWork = document.getElementById('campaign-work')
 const softwareWork = document.getElementById('software-work')
-
-const createWorkArticles = (projects) => {
-	projects.forEach((project) => {
-		let projectToAdd = document.createElement('article')
-		let imagesForAdding = project.images.map(image => `<img class="work-image" src=${image.image} alt=${image.description}/>`)
-		projectToAdd.classList.add(project.type, "project")
-		projectToAdd.innerHTML = 
-			`
-			<div class="project-topline">
-			<h3>${project.project}</h3>
-			<p class="subheader">${project.role}</p>
-			</div>
-			<p>${project.description}</p>
-			${
-				project.type === 'software' ? `<p><a href="${project.github}" target="_blank">Visit the site</a> or <a href="${project.github}" target="_blank">check out the project on Github</a></p>` : ''
-			}
-			${
-				project.type === 'campaign' ? `<p><a href="${project.github}" target="_blank">Read news coverage of the ${project.project}</a></p>` : ''
-			}
-			<div class="project-images">
-				${imagesForAdding.join('')}
-			</div>
-			`
-		workDisplay.appendChild(projectToAdd)
-	})
-}
 
 const allFilter = () => {
 	const allProjects = document.querySelectorAll(".project")
@@ -70,5 +43,3 @@ const softwareFilter = () => {
 allWork.addEventListener("click", allFilter)
 campaignWork.addEventListener("click", campaignFilter)
 softwareWork.addEventListener("click", softwareFilter)
-
-createWorkArticles(pastWork)
