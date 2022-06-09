ba7201d040bdaa087c9679dc28eed4558ce4f748
Digital Charlie <furman.jr@gmail.com>
2022-06-08 18:57:25 -0700

    toggle full desc

--- js/articles.js
+++ js/articles.js
@@ -6,7 +6,7 @@ const modalCloseButton = document.getElementById('close-modal')
 
 // This is a pretty ugly function. If you read this and have some tips for a better way to go about this, hit me up. I love learning better practices.
 const createWorkArticles = (projects) => {
-	projects.forEach((project) => {
+	projects.forEach((project, idx) => {
 		let projectToAdd = document.createElement('article')
 		let imagesForAdding = project.images.map(image => `<img class="work-image" src=${image.image} alt="${image.description}"/>`)
 		projectToAdd.classList.add(project.type, "project")
@@ -16,22 +16,36 @@ const createWorkArticles = (projects) => {
 			<h3>${project.project}</h3>
 			<p class="subheader">${project.role}</p>
 			</div>
-			<p class="project-text">${project.description}</p>
+			<p class="project-text short" id=${`proj-desc-${idx}`}>${project.description}</p>
 			${
-				project.type.includes('software') ? `<p class="project-text"><a href="${project.site}" target="_blank">Visit the site</a> or <a href="${project.github}" target="_blank">check out the project on Github</a></p>` : ''
+				project.type.includes('software') ? `<p class="project-text"><span class="show-toggle" id=${`show-${idx}`}>Show the full description</span>, <a href="${project.site}" target="_blank">visit the site</a> or <a href="${project.github}" target="_blank">check out the project on Github</a></p>` : ''
 			}
 			${
-				project.type === 'campaign' ? `<p class="project-text"><a href="${project.news}" target="_blank">Read the news coverage</a></p>` : ''
+				project.type === 'campaign' ? `<p class="project-text"><span class="show-toggle" href="" id=${`show-${idx}`}>Show the full description</span> or <a href="${project.news}" target="_blank">Read the news coverage</a></p>` : ''
 			}
 			<div class="project-images">
 				${imagesForAdding.join('')}
 			</div>
 			`
 		workDisplay.appendChild(projectToAdd)
+		document.getElementById(`show-${idx}`).addEventListener("click", ()=>toggleShow(idx))
 	})
 	const images = document.getElementsByClassName('work-image')
 	for (const image of images) {
 		image.addEventListener("click", (e)=>openModal(e))
+	}
+}
+
+const toggleShow = (idx) => {
+	console.log(idx)
+	const desc = document.getElementById(`proj-desc-${idx}`)
+	const toggle = document.getElementById(`show-${idx}`)
+	if (desc.classList.contains('short')) {
+		desc.classList.remove('short')
+		toggle.innerText = "Hide full description"
+	} else {
+		desc.classList.add('short')
+		toggle.innerText = "Show full description"
 	}
 }
 
